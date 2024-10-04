11efb8e89b3dc5871e4d4ab0162df950b54aa214
Audrey Bernier Larose <abernier@my.centennialcollege.ca>
2024-10-03 23:10:39 -0400

    [Idea] Initial procedurally generated map
    Idea on how to implement the procedurally generated map.

---
+++ Assets/_Scripts/Map/Tiles/ContourTile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContourTile : Tile
+{
+    //Add properties that are specific to contour tiles...
+    
+}
